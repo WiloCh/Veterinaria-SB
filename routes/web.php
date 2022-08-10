480c755b460b1a3f9b0dfdc1231aac08f389516a
Apvasquez5 <apvasquez5@espe.edu.ec>
2022-08-09 21:10:11 -0500

    Inicio proyecto

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::resource('cliente', ClienteController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
